4e22648de81b4b60afe109cc2988ddde48930528
hazemali1 <hazemali1343@gmail.com>
2023-08-07 09:36:44 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,30 @@ class BaseModel:
         """
         Instantiation
         """
-        if kwarg:
-            format_data = "%y-/%m-/%dT%H:%M:%S.%f"
+        if kwargs:
             for k, v in kwargs.items():
                 if k == "__class__":
                     continue
-                if k == "created_at":
-                    self.created_at = datetime.strptime\
-                            (kwargs["created_at"], format_data)
-                if k == "updated_at":
-                    self.updated_at = datetime.strptime\
-                            (kwargs["updated_at"], format_data)
+                if k == "updated_at" or k == "created_at":
+                    self.__dict__[k] = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, k. v)
+                    setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, (self.id),
+            self.__dict__)
+
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        return self.__dict__
+
