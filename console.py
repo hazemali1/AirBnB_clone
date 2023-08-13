e9c12f99bcbe04547c143a447c0f636c8f544945
Dr_Omar <Dr.omartalat@gmail.com>
2023-08-13 18:08:01 +0300

    finished isa

--- console.py
+++ console.py
@@ -7,8 +7,6 @@ import re
 import cmd
 from models import storage
 from models.engine.file_storage import class_dict
-from models.base_model import BaseModel
-from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
@@ -120,7 +118,7 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             for k, v in date_base.items():
-                if v.id == q[1]:
+                if v.id == q[1] and v.__class__.__name__ == q[0]:
                     b = 2
                     print(v)
             if b == 1:
